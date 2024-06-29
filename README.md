aef9e9a121cb7560b43d99e8c7cfe31b0b467b90
Angel Serrato <serratoarias@yahoo.com>
2024-06-28 21:42:24 -0500

    Organizo proyecto

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to fix the req.flash() is not a function error
+
+https://www.reddit.com/r/node/comments/4aylzz/javascript_typeerror_reqflash_is_not_a_function/
+
+https://www.youtube.com/watch?v=PAm_QcN6Ffs
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+const flash = require('connect-flash');
+const session = require('express-session')
+const dotenv = require('dotenv')
+const connectDB = require('./server/config/db')
+
+dotenv.config()
+connectDB()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// Middleware que interpreta datos codificados en url y los en expone en req.body
+// extended: true, permite que los valores en los formularios puedan ser de cualquier tipo
+app.use(express.urlencoded({ extended: true }))
+// Middleware que parsea los datos JSON enviados en el cuerpo de las solicitudes
+app.use(express.json())
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        }
+    })
+)
+app.use(flash({ sessionKeyName: 'flashMessage' }))
+app.use(express.static('public'))
+app.use(expressLayout)
+
+app.use('/', require('./server/routes/customer'))
+
+app.set('view engine', 'ejs')
+app.set('layout', './layouts/main')
+
+app.get('*', (req, res) => {
+    res.status(404).render('404', { title: 'Not found' })
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server listening on: http://localhost:${PORT}`)
+})
